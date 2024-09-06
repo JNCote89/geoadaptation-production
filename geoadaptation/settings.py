8cf0b53f00222e23d9b33a87be38910bb9ce49ec
JNCote89 <Jean-Nicolas.Cote@USherbrooke.ca>
2024-09-06 16:18:13 -0400

    phd vulnerability dashboard prototype

--- geoadaptation/settings.py
+++ geoadaptation/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'coresite.apps.CoresiteConfig',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
